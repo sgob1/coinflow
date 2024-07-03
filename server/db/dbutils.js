e5048d3888cfcf4424bb7b212c624e17ee50c67d
sgob <marco.sgobino@gmail.com>
2024-07-03 09:20:57 +0200

    Refactored dbutils.js
    Reformatted code

--- server/db/dbutils.js
+++ server/db/dbutils.js
@@ -2,8 +2,8 @@ const MONGODB_URI_SCHEME = "mongodb://";
 
 let uriComposer = function (addr, port, dbname) {
   return `${MONGODB_URI_SCHEME}${addr}:${port}/${dbname}`;
-}
+};
 
 module.exports = {
   uriComposer,
-}
+};
