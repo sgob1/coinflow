bb450e945f262058774412e870428adcac643d3c
sgob <marco.sgobino@gmail.com>
2024-07-08 15:45:46 +0200

    Streamlined LoginForm.vue component

--- client/coinflow/src/components/LoginForm.vue
+++ client/coinflow/src/components/LoginForm.vue
@@ -5,24 +5,7 @@ const result = ref([])
 const username = defineModel('username')
 const password = defineModel('password')
 
-async function signup() {
-  console.log('Submitting user infos!')
-  const results = await fetch('/api/auth/signup', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      username: 'john',
-      password: 'notapass',
-      name: 'John',
-      surname: 'Doe'
-    })
-  })
-  result.value = await results.json()
-  console.log(result.value)
-}
-
 async function signin() {
-  console.log({ username: username.value, password: password.value })
   const res = await fetch('/api/auth/signin', {
     method: 'POST',
     headers: {
@@ -34,12 +17,6 @@ async function signin() {
   console.log(data)
 }
 
-async function balance() {
-  console.log('Balance!')
-  const results = await fetch('/api/balance')
-  result.value = await results.json()
-  console.log(result.value)
-}
 </script>
 
 <template>
@@ -50,8 +27,5 @@ async function balance() {
     <input v-model="password" placeholder="password" type="password" />
     <input type="submit" value="log in" />
   </form>
-  <button @click="signup">Signup!</button>
-  <button @click="signin">Signin!</button>
-  <button @click="balance">Balance!</button>
   <p>{{ result }}</p>
 </template>
