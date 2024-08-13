f1c44ae8260f78d9314a86071973856aaaa685c6
sgob <marco.sgobino@gmail.com>
2024-08-13 10:34:20 +0200

    Added skeleton for quotas section

--- client/coinflow/src/components/TransactionEditorComponent.vue
+++ client/coinflow/src/components/TransactionEditorComponent.vue
@@ -1,25 +1,51 @@
 <template>
   <div class="editor-wrapper">
+    <h1>Description</h1>
     <input
       type="text"
       v-model="currentTransaction.description"
       value="currentTransaction.description"
     />
+    <h1>Category</h1>
     <input type="text" v-model="currentTransaction.category" value="currentTransaction.category" />
+    <h1>Date</h1>
     <div class="date-editor-wrapper">
       <input type="text" v-model="currentTransaction.year" value="currentTransaction.year" />
       <input type="text" v-model="currentTransaction.month" value="currentTransaction.month" />
       <input type="text" v-model="currentTransaction.day" value="currentTransaction.day" />
+    </div>
+    <h1>User Quotas</h1>
+    <div v-if="dataReady">Cached users: {{ cachedUsers }}</div>
+    <div class="user-quotas-editor-wrapper">
+      <select v-model="selectedUsername">
+        <option v-for="user in cachedUsers" :key="user.username">
+          {{ user.username }}
+        </option>
+      </select>
+      <div>Total cost: {{ totalCost }}</div>
+      <div v-for="(quota, key) in currentTransaction.quotas" :key="quota">
+        {{ key }}:
+        <input
+          type="text"
+          v-model="currentTransaction.quotas[key]"
+          value="currentTransaction.quotas[key]"
+        />
+      </div>
     </div>
     {{ currentTransaction }}
   </div>
 </template>
 
 <script>
+import fetcher from '@/utils/fetch/fetcher.js'
+
 export default {
   data() {
     return {
-      currentTransaction: {}
+      currentTransaction: {},
+      selectedUsername: '',
+      cachedUsers: [],
+      dataReady: false
     }
   },
   props: {
@@ -28,12 +54,31 @@ export default {
   methods: {
     reset() {
       this.currentTransaction = {}
+    },
+    submit() {
+      // TODO: parse transaction object and clean it if it contains spurious items
+    }
+  },
+  computed: {
+    totalCost() {
+      if (!this.currentTransaction.quotas) return 0
+
+      let totalCost = 0
+      for (let quota in this.currentTransaction.quotas) {
+        totalCost = totalCost + Number(this.currentTransaction.quotas[quota])
+      }
+      return totalCost
     }
   },
   watch: {
     transaction: function (newVal, curVal) {
       Object.assign(this.currentTransaction, newVal)
     }
+  },
+  async mounted() {
+    this.cachedUsers = await fetcher.usersSearch('')
+    console.log(this.cachedUsers)
+    this.dataReady = true
   }
 }
 </script>
@@ -48,5 +93,8 @@ export default {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+}
+input {
+  font-size: 1.1em;
 }
 </style>
