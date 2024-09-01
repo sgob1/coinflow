606ea6df50a48b00e33b6d99dae8a6d1ad179cda
sgob <marco.sgobino@gmail.com>
2024-09-01 15:26:31 +0200

    Updated install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -5,8 +5,10 @@ cd ./server
 echo "Installing server node packages..."
 npm install
 cd ..
-cd client/coinflow
+cd client
 echo "Installing client node packages..."
+npm install
+cd coinflow
 npm install
 echo "Building project..."
 npm run format-build
